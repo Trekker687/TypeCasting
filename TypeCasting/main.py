bbce751ccf3b3024a1230b95f3d780ca3dac5e72
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-10-28 17:44:38 +0530

    Add files via upload

---
+++ TypeCasting/main.py
@@ -0,0 +1,18 @@
+name = "Manvir"
+print(name)
+print(type(name))
+
+age = 300
+age = str(age) #Typecasting int to str 
+print(age)
+print(type(age))
+
+weight = 40.1256921258731284832
+weight = int(weight) #Typecasting float to int 
+print(weight)
+print(type(weight))
+
+very_cool = True
+very_cool = str(very_cool) #Typecasting bool to str
+print(very_cool)
+print(type(very_cool))
